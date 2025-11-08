544e39594a77576de71be71abdc0e0708814c122
anantoniuk <Antoniuk.an17@gmail.com>
2025-11-08 10:34:16 +0000

    Deploying to gh-pages from @ anantoniuk/-nice-view@c6c432b7718195b3a83b15f5483c6a4e205e74b7 🚀

---
+++ index.6cce1b0b.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-modal-hero-open]"),o=document.querySelector("[data-modal-hero]"),t=document.querySelector("[data-modal-hero-close]"),c=function(){o.classList.toggle("is-hidden-hero"),document.body.classList.toggle("no__scroll-hero")};e.addEventListener("click",c),t.addEventListener("click",c)}();
+//# sourceMappingURL=index.6cce1b0b.js.map
