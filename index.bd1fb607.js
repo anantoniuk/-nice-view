69802f88a2ea2d8f129c0d7a366e109d2d1e696f
anantoniuk <Antoniuk.an17@gmail.com>
2025-11-08 18:16:27 +0000

    Deploying to gh-pages from @ anantoniuk/-nice-view@735e297c614e90541fa3227f1840390b41a8385b 🚀

--- index.bd1fb607.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("[data-modal-hero-open]"),o=document.querySelector("[data-modal-hero]"),t=document.querySelector("[data-modal-hero-close]"),d=()=>{o.classList.toggle("is-hidden-hero"),document.body.classList.toggle("no__scroll-hero")};e.addEventListener("click",d),t.addEventListener("click",d);
-//# sourceMappingURL=index.bd1fb607.js.map
