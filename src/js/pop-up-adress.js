73801e5cf5e7dca477a25c5917a96cda383381fb
Alika Beliaeva <alikabelxar2020@gmail.com>
2025-11-30 13:06:21 +0200

    done-blockers

--- src/js/pop-up-adress.js
+++ src/js/pop-up-adress.js
@@ -4,13 +4,10 @@ const modal = document.querySelector('[data-modal-pop]');
 const subscribeContent = document.querySelector('[data-form-content]');
 const closeBtn = document.querySelector('[data-modal-pop-adress-close]');
 
-let closeTimer;
-
 console.log(closeBtn);
 const handlePopupClose = event => {
-  console.log('123');
   if (subscribeContent && !subscribeContent.contains(event.target)) {
-   startClosing()
+    popup.close();
   }
 };
 
@@ -20,33 +17,9 @@ const handleSubmit = event => {
   document.body.classList.add('no__scroll-pop-adress');
   popup.showModal();
   event.target.reset();
-  clearTimeout(closeTimer);
-
-  // subscribeContent?.classList.add('subscribe-popup__content--animate');
-
-  // closeTimer = setTimeout(startClosing, 5000);
 };
 
-function startClosing() {
-  if (!popup.open) return;
-
-  if (subscribeContent?.classList.contains('closing')) return;
-
-  subscribeContent?.classList.add('closing');
-
-  const onAnimEnd = () => {
-    subscribeContent?.classList.remove('closing');
-    subscribeContent?.classList.remove('subscribe-popup__content--animate');
-    popup.close();
-    subscribeContent?.removeEventListener('animationend', onAnimEnd);
-    subscribeContent?.removeEventListener('transitionend', onAnimEnd);
-  };
-
-  subscribeContent?.addEventListener('animationend', onAnimEnd);
-  subscribeContent?.addEventListener('transitionend', onAnimEnd);
-}
-
-popup?.addEventListener('click', handlePopupClose);
+// popup?.addEventListener('click', handlePopupClose);
 subscribeForm?.addEventListener('submit', handleSubmit);
 
 closeBtn.addEventListener('click', handlePopupClose);
